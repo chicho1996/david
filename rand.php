ae4d794370d9f71b3b4d15bf1dde400d2ac3bd81
chicho1996 <dato.chicho@yahoo.com>
2016-04-22 13:48:55 +0400

    Update rand.php

--- rand.php
+++ rand.php
@@ -1,14 +1,25 @@
 <?php
-$check;
-$number;
-$rand=array();
-while(sizeof($rand)<5){
-		$number=rand(1,5);
-	if(!(in_array($number,$rand)))
+function random($min,$max,$lengthLimit=null){
+	$check;
+	$number;
+	$length=$max-$min+1;
 
-	array_push($rand, $number);
+	if ($lengthLimit != null)
+		$length = $lengthLimit;	
 
+	$rand=array();
+	while(sizeof($rand)<$length){
+			$number=rand($min,$max);
+		if(!(in_array($number,$rand)))
+		array_push($rand, $number);
+	}
+
+	return $rand;
 }
-print_r($rand);
 
+$data = random(1,5);
+$dataLimit = random(1, 5, 2);
+
+print_r( $data );
+print_r( $dataLimit );
 ?>
